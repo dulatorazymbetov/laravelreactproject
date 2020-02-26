e01eff223f18a3bc918277943f4985296431da31
Рауан К <rauank83@gmail.com>
2020-02-26 11:25:48 +0000

    Update 2020_02_25_140325_create_employees_table.php

--- database/migrations/2020_02_25_140325_create_employees_table.php
+++ database/migrations/2020_02_25_140325_create_employees_table.php
@@ -15,6 +15,7 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+			$table->tinyInteger('post_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
