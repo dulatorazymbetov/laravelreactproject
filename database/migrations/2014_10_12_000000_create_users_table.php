0b0e737a7c5ca4feb5aa3638378493f8d053c562
Рауан К <rauank83@gmail.com>
2020-02-27 09:29:28 +0000

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,7 +28,7 @@ class CreateUsersTable extends Migration
             $table->date('birthdate')->nullable();
             $table->string('iin')->nullable();
 			
-			$table->boolean('foreigner')->default('false'); //прибывший из-за рубежа
+			$table->boolean('foreigner')->default('false')->after('gender'); 
 			
 			
 			
