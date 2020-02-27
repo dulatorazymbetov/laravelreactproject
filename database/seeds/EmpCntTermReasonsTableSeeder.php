3301ea6b7980a43b483be0b83c6ac63f6c2ae7fb
Рауан К <rauank83@gmail.com>
2020-02-27 17:52:49 +0000

    Update EmpCntTermReasonsTableSeeder.php

--- database/seeds/EmpCntTermReasonsTableSeeder.php
+++ database/seeds/EmpCntTermReasonsTableSeeder.php
@@ -11,6 +11,24 @@ class EmpCntTermReasonsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //emp_contract_term_reasons
+        DB::table('emp_contract_term_reasons')->insert([
+            'id' => '1',
+            'description_kk' => 'По собственному желанию',
+            'description_ru' => 'По собственному желанию',
+            'description_en' => 'По собственному желанию'
+        ]);
+        DB::table('emp_contract_term_reasons')->insert([
+            'id' => '2',
+            'description_kk' => 'В связи с истечением срока трудового договора',
+            'description_ru' => 'В связи с истечением срока трудового договора',
+            'description_en' => 'В связи с истечением срока трудового договораю'
+        ]);
+        DB::table('emp_contract_term_reasons')->insert([
+            'id' => '3',
+            'description_kk' => 'В связи с нарушением условий трудового договора',
+            'description_ru' => 'В связи с нарушением условий трудового договора',
+            'description_en' => 'В связи с нарушением условий трудового договора'
+        ]);
     }
 }
