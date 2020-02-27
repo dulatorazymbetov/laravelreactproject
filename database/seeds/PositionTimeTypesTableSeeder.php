2fc22b6e932527c1da9e424952aa8a7adcbe43ad
Рауан К <rauank83@gmail.com>
2020-02-27 20:28:32 +0000

    Update PositionTimeTypesTableSeeder.php

--- database/seeds/PositionTimeTypesTableSeeder.php
+++ database/seeds/PositionTimeTypesTableSeeder.php
@@ -12,5 +12,23 @@ class PositionTimeTypesTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('position_time_types')->insert([
+            'id' => '1',
+            'description_kk' => 'Штатный',
+            'description_ru' => 'Штатный',
+            'description_en' => 'Штатный'
+        ]);
+        DB::table('position_time_types')->insert([
+            'id' => '2',
+            'description_kk' => 'Внутренний совместитель',
+            'description_ru' => 'Внутренний совместитель',
+            'description_en' => 'Внутренний совместитель'
+        ]);
+        DB::table('position_time_types')->insert([
+            'id' => '3',
+            'description_kk' => 'Совместитель со стороны',
+            'description_ru' => 'Совместитель со стороны',
+            'description_en' => 'Совместитель со стороны'
+        ]);
     }
 }
