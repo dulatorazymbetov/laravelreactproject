c4c4a53def512e510bde9cffab414abd9c5c854c
Рауан К <rauank83@gmail.com>
2020-02-27 17:27:14 +0000

    Update AcademicDegreesTableSeeder.php

--- database/seeds/AcademicDegreesTableSeeder.php
+++ database/seeds/AcademicDegreesTableSeeder.php
@@ -12,42 +12,42 @@ class AcademicDegreesTableSeeder extends Seeder
     public function run()
     {
         //
-         DB::table('study_statuses')->insert([
+         DB::table('academic_degrees')->insert([
             'sort_order' => '1',
             'description_kk' => 'Жоқ',
             'description_ru' => 'Не имеет степени',
             'description_en' => 'None'
         ]);
         
-        DB::table('study_statuses')->insert([
+        DB::table('academic_degrees')->insert([
             'sort_order' => '2',
             'description_kk' => 'Магистр',
             'description_ru' => 'Магистр',
             'description_en' => 'Master'
         ]);
         
-        DB::table('study_statuses')->insert([
+        DB::table('academic_degrees')->insert([
             'sort_order' => '3',
             'description_kk' => 'Ғылымдар кандидаты',
             'description_ru' => 'Кандидат наук',
             'description_en' => 'Кандидат наук'
         ]);
 
-        DB::table('study_statuses')->insert([
+        DB::table('academic_degrees')->insert([
             'sort_order' => '4',
             'description_kk' => 'Ғылымдар докторы',
             'description_ru' => 'Доктор наук',
             'description_en' => 'Доктор наук'
         ]);
         
-        DB::table('study_statuses')->insert([
+        DB::table('academic_degrees')->insert([
             'sort_order' => '5',
             'description_kk' => 'Доктор PhD',
             'description_ru' => 'Доктор PhD',
             'description_en' => 'Доктор PhD'
         ]);
         
-        DB::table('study_statuses')->insert([
+        DB::table('academic_degrees')->insert([
             'sort_order' => '6',
             'description_kk' => 'Доктор по профилю',
             'description_ru' => 'Доктор по профилю',
