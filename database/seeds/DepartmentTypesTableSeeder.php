4a99b8b2a8f2b8928b22ae2afee33307da7157ae
Рауан К <rauank83@gmail.com>
2020-02-27 20:25:53 +0000

    Update DepartmentTypesTableSeeder.php

--- database/seeds/DepartmentTypesTableSeeder.php
+++ database/seeds/DepartmentTypesTableSeeder.php
@@ -12,5 +12,29 @@ class DepartmentTypesTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('department_types')->insert([
+            'id' => '1',
+            'description_kk' => 'Факультет',
+            'description_ru' => 'Факультет',
+            'description_en' => 'Faculty'
+        ]);
+        DB::table('department_types')->insert([
+            'id' => '2',
+            'description_kk' => 'Кафедра',
+            'description_ru' => 'Кафедра',
+            'description_en' => 'Cafedra'
+        ]);
+        DB::table('department_types')->insert([
+            'id' => 3',
+            'description_kk' => 'Департамент',
+            'description_ru' => 'Департамент',
+            'description_en' => 'Department'
+        ]);
+        DB::table('department_types')->insert([
+            'id' => '4',
+            'description_kk' => 'Отдел',
+            'description_ru' => 'Отдел',
+            'description_en' => 'Division'
+        ]);
     }
 }
