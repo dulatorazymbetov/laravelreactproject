25c428176e62d80cbb15b7cf39b8135dd3ca8c34
Рауан К <rauank83@gmail.com>
2020-02-27 16:36:24 +0000

    Update TeachingLanguagesTableSeeder.php

--- database/seeds/TeachingLanguagesTableSeeder.php
+++ database/seeds/TeachingLanguagesTableSeeder.php
@@ -12,5 +12,26 @@ class TeachingLanguagesTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('teaching_languages')->insert([
+            'id' => '1',
+            'name' => 'kz',
+            'description_kk' => 'Qazaqsha',
+            'description_ru' => 'Казахский',
+            'description_en' => 'Kazakh'
+        ]);
+        DB::table('teaching_languages')->insert([
+            'id' => '2',
+            'name' => 'ru',
+            'description_kk' => 'Орысша',
+            'description_ru' => 'Русский',
+            'description_en' => 'Russian'
+        ]);
+        DB::table('teaching_languages')->insert([
+            'id' => '3',
+            'name' => 'en',
+            'description_kk' => 'Aǵylshyn',
+            'description_ru' => 'Английский',
+            'description_en' => 'English'
+        ]);
     }
 }
