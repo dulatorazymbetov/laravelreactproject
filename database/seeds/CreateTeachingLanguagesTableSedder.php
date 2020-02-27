6a665bd5df1c9f6cb34945af71e2eb88e2b1e5d7
root <root@newcampus.iitu.kz>
2020-02-27 22:29:31 +0600

    Add teaching lang records

---
+++ database/seeds/CreateTeachingLanguagesTableSedder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CreateTeachingLanguagesTableSedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
