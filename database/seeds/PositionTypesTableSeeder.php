6f44f45e107713d0fbd2f1887a7ef34786bb7555
Рауан К <rauank83@gmail.com>
2020-02-27 20:32:59 +0000

    Update PositionTypesTableSeeder.php

--- database/seeds/PositionTypesTableSeeder.php
+++ database/seeds/PositionTypesTableSeeder.php
@@ -12,5 +12,41 @@ class PositionTypesTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('position_types')->insert([
+            'id' => '1',
+            'staff_type_id' => '1',
+            'description_kk' => 'Ректор',
+            'description_ru' => 'Ректор',
+            'description_en' => 'Rector'
+        ]);
+        DB::table('position_types')->insert([
+            'id' => '2',
+            'staff_type_id' => '1',
+            'description_kk' => 'Первый проректор',
+            'description_ru' => 'Первый проректор',
+            'description_en' => 'Первый проректор'
+        ]);
+        DB::table('position_types')->insert([
+            'id' => '3',
+            'staff_type_id' => '1',
+            'description_kk' => 'Заведующая кафедрой',
+            'description_ru' => 'Заведующая кафедрой',
+            'description_en' => 'Заведующая кафедрой'
+        ]);
+        DB::table('position_types')->insert([
+            'id' => '4',
+            'staff_type_id' => '1',
+            'description_kk' => 'Преподаватель',
+            'description_ru' => 'Преподаватель',
+            'description_en' => 'Teacher'
+        ]);
+                DB::table('position_types')->insert([
+            'id' => '5',
+            'staff_type_id' => '1',
+            'description_kk' => 'Инженер',
+            'description_ru' => 'Инженер',
+            'description_en' => 'Engineer'
+        ]);
+
     }
 }
