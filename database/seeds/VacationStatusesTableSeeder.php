40407c4012c5c7c204c6f85a4df50a9fd9a0dee5
Рауан К <rauank83@gmail.com>
2020-02-27 18:45:20 +0000

    Update VacationStatusesTableSeeder.php

--- database/seeds/VacationStatusesTableSeeder.php
+++ database/seeds/VacationStatusesTableSeeder.php
@@ -12,37 +12,37 @@ class VacationStatusesTableSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '1',
             'description_kk' => 'Ежегодный трудовой отпуск',
             'description_ru' => 'Ежегодный трудовой отпуск',
             'description_en' => 'Ежегодный трудовой отпуск'
         ]);
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '2',
             'description_kk' => 'Отпуск без содержания',
             'description_ru' => 'Отпуск без содержания',
             'description_en' => 'Отпуск без содержания'
         ]);
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '3',
             'description_kk' => 'Отпуск по беременности и родам',
             'description_ru' => 'Отпуск по беременности и родам',
             'description_en' => 'Отпуск по беременности и родам'
         ]);
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '4',
             'description_kk' => 'Отпуск по уходу за ребенком до 3-х лет',
             'description_ru' => 'Отпуск по уходу за ребенком до 3-х лет',
             'description_en' => 'Отпуск по уходу за ребенком до 3-х лет'
         ]);
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '5',
             'description_kk' => 'Твороческий отпуск',
             'description_ru' => 'Твороческий отпуск',
             'description_en' => 'Твороческий отпуск'
         ]);
-        DB::table('study_statuses')->insert([
+        DB::table('vacation_statuses')->insert([
             'id' => '6',
             'description_kk' => 'Отпуск для прохождения службы в ВС РК',
             'description_ru' => 'Отпуск для прохождения службы в ВС РК',
