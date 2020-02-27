4f9603426f3ad302271031244090c2d070532b2a
Рауан К <rauank83@gmail.com>
2020-02-27 17:35:35 +0000

    Update EnglishLevelsTableSeeder.php

--- database/seeds/EnglishLevelsTableSeeder.php
+++ database/seeds/EnglishLevelsTableSeeder.php
@@ -12,5 +12,49 @@ class EnglishLevelsTableSeeder extends Seeder
     public function run()
     {
         //
+         DB::table('english_levels')->insert([
+            'name' => 'A1',
+            'description_kk' => 'A1',
+            'description_ru' => 'A1',
+            'description_en' => 'A1'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'A2',
+            'description_kk' => 'A2',
+            'description_ru' => 'A2',
+            'description_en' => 'A2'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'B1',
+            'description_kk' => 'B1',
+            'description_ru' => 'B1',
+            'description_en' => 'B1'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'B2',
+            'description_kk' => 'B2',
+            'description_ru' => 'B2',
+            'description_en' => 'B2'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'C1',
+            'description_kk' => 'C1',
+            'description_ru' => 'C1',
+            'description_en' => 'C1'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'C2',
+            'description_kk' => 'C2',
+            'description_ru' => 'C2',
+            'description_en' => 'C2'
+        ]);
+        DB::table('english_levels')->insert([
+            'name' => 'None',
+            'description_kk' => 'None',
+            'description_ru' => 'None',
+            'description_en' => 'None'
+        ]);
+
+
     }
 }
