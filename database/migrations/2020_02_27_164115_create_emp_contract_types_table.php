91736d535cdefb3d81f8e4dc6936d460a57f800d
Рауан К <rauank83@gmail.com>
2020-02-27 16:59:16 +0000

    Update 2020_02_27_164115_create_emp_contract_types_table.php

--- database/migrations/2020_02_27_164115_create_emp_contract_types_table.php
+++ database/migrations/2020_02_27_164115_create_emp_contract_types_table.php
@@ -15,7 +15,11 @@ class CreateEmpContractTypesTable extends Migration
     {
         Schema::create('emp_contract_types', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('description_kk')->nullable();
+			$table->string('description_ru')->nullable();
+			$table->string('description_en')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
