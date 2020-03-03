72aeee458fb73819384a82aecdad3d75865b0474
aibek.mazhenov <aibek.mazhenov@gmail.com>
2020-03-04 01:51:30 +0600

    Фикс модели staff

--- database/migrations/2020_02_25_140325_create_staff_table.php
+++ database/migrations/2020_02_25_140325_create_staff_table.php
@@ -4,10 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStaffsTable extends Migration
+class CreateStaffTable extends Migration
 {
     public function up(){
-        Schema::create('staffs', function (Blueprint $table) {
+        Schema::create('staff', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id');
 			$table->boolean('foreigner')->default(false); // прибывший из-за рубежа
@@ -43,6 +43,6 @@ class CreateStaffsTable extends Migration
         });
     }
     public function down(){
-        Schema::dropIfExists('staffs');
+        Schema::dropIfExists('staff');
     }
 }
