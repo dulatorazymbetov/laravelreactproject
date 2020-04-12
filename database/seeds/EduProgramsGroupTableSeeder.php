fecbc35a51c5abc8b6382d7cf1bc00e5f4d681eb
Rauan K. Kuanysh <rkuanysh@iitu.kz>
2020-04-12 23:20:38 +0600

    EduProgramGroup

--- database/seeds/EduProgramsGroupTableSeeder.php
+++ database/seeds/EduProgramsGroupTableSeeder.php
@@ -13,7 +13,7 @@ class EduProgramsGroupTableSeeder extends Seeder
     {
         DB::table('edu_programs_groups')->insert([
 		'id' => '1827',
-		'training_direction_id' => '2'
+		'training_direction_id' => '2',
 		'code' => 'B001',
 		'title_kk' => 'Педагогика және психология',
 		'title_ru' => 'Педагогика и психология',
@@ -22,7 +22,7 @@ class EduProgramsGroupTableSeeder extends Seeder
 		
 		DB::table('edu_programs_groups')->insert([
 		'id' => '1828',
-		'training_direction_id' => '3'
+		'training_direction_id' => '3',
 		'code' => 'B002',
 		'title_kk' => 'Мектепке дейінгі оқыту және тәрбиелеу',
 		'title_ru' => 'Дошкольное обучение и воспитание',
@@ -31,7 +31,7 @@ class EduProgramsGroupTableSeeder extends Seeder
 		
 		DB::table('edu_programs_groups')->insert([
 		'id' => '1829',
-		'training_direction_id' => '4'
+		'training_direction_id' => '4',
 		'code' => 'B003',
 		'title_kk' => 'Бастауышта оқыту педагогикасы мен әдістемесі',
 		'title_ru' => 'Педагогика и методика начального обучения',
