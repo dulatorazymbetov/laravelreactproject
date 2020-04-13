a111b855cdcfc2117652e0d7c3513498179c79e8
aibek.mazhenov <aibek.mazhenov@gmail.com>
2020-04-14 02:40:42 +0600

    Ввод образовательных программ (Попытка исправить глюк с moment.js)

--- resources/js/layout/datepicker.js
+++ resources/js/layout/datepicker.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 import moment from "moment";
-import "moment/locale/ru";
-moment.locale("ru");
+//import "moment/locale/ru";
+//moment.locale("ru");
 
 import { MuiPickersUtilsProvider, DatePicker} from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
