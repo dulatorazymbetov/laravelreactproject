779827cf66578750f77fa4263d85921ae63be2f9
Rauan K. Kuanysh <rkuanysh@iitu.kz>
2020-04-28 03:43:27 +0600

    EduProgrammsSeeder

---
+++ database/seeds/EduProgramsTableSeeder.php
@@ -0,0 +1,181 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EduProgramsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+		
+DB::insert("
+insert into edu_programs (title_ru) VALUES (' Компьютерные науки(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Аналитика больших данных(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Бизнес-анализ(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Информационные системы(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('ERP-системы(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Киберфизические системы(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Наука о данных и Машинное обучение(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Сетевое и системное администрирование(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Программная инженерия(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES (' Финансовая Математика(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Data science(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Инженерная Математика(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Компьютерная безопасность(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Аппаратные средства защиты информации(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Сетевая безопасность(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Графические вычисления(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Телекоммуникационные системы и сети(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Радиотехнические системы передачи информации(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Информатика и организация цифровизации образования(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Компьютерная физика(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Электронный бизнес(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Инновационный менеджмент(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Финансовый инжиниринг(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Финансовые технологии(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Бизнес-журналистика, SMM & PR(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Цифровая журналистика(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Международная журналистика и интернет-безопасность(реестр_4г) ');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Телерадиожурналистика(реестр_4г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES (' Программная инженерия(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Компьютерная безопасность(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Сетевая безопасность(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Бизнес-аналитика(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Управление IT-проектами(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Инженерная Математика(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Наука о данных(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Телекоммуникационные системы и сети(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Цифровые медиатехнологии визуализация данных(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Проектный менеджмент(реестр_2г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Интеллектуальные системы(реестр_3г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Компьютерная и программная инженерия(реестр_3г)');
+");
+
+DB::insert("
+insert into edu_programs (title_ru) VALUES ('Компьютинг и наука о данных(реестр_2г)');
+");
+		
+    }
+}
