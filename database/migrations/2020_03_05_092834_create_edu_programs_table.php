1004030909bf4b05c6cdc7f51232e03e5e7a6204
aibek.mazhenov <aibek.mazhenov@gmail.com>
2020-04-28 11:09:48 +0600

    Изменение полей в БД ОП

--- database/migrations/2020_03_05_092834_create_edu_programs_table.php
+++ database/migrations/2020_03_05_092834_create_edu_programs_table.php
@@ -14,23 +14,22 @@ class CreateEduProgramsTable extends Migration
     public function up()
     {
         Schema::create('edu_programs', function (Blueprint $table) {
-                $table->bigIncrements('id');
+            $table->bigIncrements('id');
 			$table->string('reg_num'); //шифр. регистрационный номер.
             $table->string('title_kk');
-                $table->string('title_ru');
+            $table->string('title_ru');
             $table->string('title_en')->nullable();
 			$table->integer('edu_programs_type_id');
 			
 			$table->integer('edu_programs_group_id');	//Группа образовательных программ	//edu_programs_groups
-			$table->string('goals_kk')->nullable();	//Цели ОП
-			$table->string('goals_ru')->nullable();
-			$table->string('goals_en')->nullable();
+			$table->text('goals_kk')->nullable();	//Цели ОП
+			$table->text('goals_ru')->nullable();
+			$table->text('goals_en')->nullable();
 			$table->integer('academic_degree_id');	//Академическая степень
 			$table->float('duration');	//продолжительность обучения в годах
 			$table->integer('credits');	//кредиты
 			$table->integer('study_language_id');	//язык обучения
-			    $table->date('app_date');	//дата утверждения ОП
-			
+			$table->date('app_date');	//дата утверждения ОП
 			
 			$table->tinyInteger('nrk_level');
 			$table->tinyInteger('ork_level');
